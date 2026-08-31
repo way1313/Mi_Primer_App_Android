4e11c9dcb1336597204bf8a95265878a2438f995
Wilfer Yepes <wilferyepes@hotmail.com>
2026-08-31 16:13:11 -0500

    Primera Aplicacion Android

---
+++ app/src/main/java/com/example/miprimerappandroid/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.miprimerappandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.miprimerappandroid.ui.theme.MiPrimerAppAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MiPrimerAppAndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    saludo(
+                        name = "Grupo Voltt",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun saludo(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hola $name!",
+        modifier = modifier
+            .padding(16.dp)
+            .background(color = Color.Black)
+            .offset(),
+        color = Color.Yellow
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun saludoVistaPrevia() {
+    MiPrimerAppAndroidTheme {
+        saludo("Grupo Voltt")
+    }
+}
